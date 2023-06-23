7b6ad1f9727ad2ba92bfd92102b3ed114cc01f14
DiegoFernando01 <diegofvictoria2013@gmail.com>
2023-06-22 20:55:57 -0500

    Update postgres credentials

--- auto_selecto/auto_selecto/settings.py
+++ auto_selecto/auto_selecto/settings.py
@@ -87,8 +87,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'auto_selecto',
-        'USER': 'auto_s',
-        'PASSWORD': 'root',
+        'USER': 'postgres',
+        'PASSWORD': 'DiegoFernando1386',
         'HOST': 'localhost',
         'DATABASE_PORT': '5432',
     }
