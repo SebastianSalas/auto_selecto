a1bc2cfbb17b6d4c510dd6f8e45ac0215c95f8ee
SebastianSalas <janiersalas09@gmail.com>
2023-06-23 02:23:33 -0500

    Adds some changes to redirect

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,20 @@
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, Navigate } from "react-router-dom";
 import PrivateRoute from "./utils/PrivateRoutes";
 
 import HomePage from "./pages/HomePage";
 import LoginPage from "./pages/LoginPage";
 import NotFoundPage from "./pages/NotFoundPage";
 import SignupPage from "./pages/SignupPage";
+import AuthContext from "./context/AuthContext";
+import { useContext } from "react";
 
 export default function App() {
+  let {user} = useContext(AuthContext)
   return (
     <Routes>
-      <Route path="/" element={<PrivateRoute />}>
-        <Route path="/" element={<HomePage />} />
-      </Route>
+      <Route path="/" element={
+        !user ? <Navigate to="/login" /> : <HomePage />
+      } />
       <Route path="/login" element={<LoginPage />} />
       <Route path="/sign_up" element={<SignupPage />} />
       <Route path="*" element={<NotFoundPage />} />
--- client/src/context/AuthContext.jsx
+++ client/src/context/AuthContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState, useEffect } from "react";
+import { createContext, useState } from "react";
 import jwt_decode from "jwt-decode";
 import { useNavigate } from 'react-router-dom'
 
