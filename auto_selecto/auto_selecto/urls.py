8017f99c5d6f683af5223d19af3312e7a7cec020
SebastianSalas <janiersalas09@gmail.com>
2023-06-26 00:30:50 -0500

    Adds company position model

--- auto_selecto/auto_selecto/urls.py
+++ auto_selecto/auto_selecto/urls.py
@@ -6,4 +6,5 @@ from django.views.generic import TemplateView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('users.api.urls')),
+    path('api/', include('concesionariaApp.urls')),
 ]
--- auto_selecto/concesionariaApp/models.py
+++ auto_selecto/concesionariaApp/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.utils import timezone
-
+from django.db import transaction
 
 class Office(models.Model):
   name = models.CharField(max_length=60)
@@ -12,4 +12,25 @@ class Office(models.Model):
 
   def __str__(self):
     return f"id: {self.id}, name: {self.name}, address: {self.address}, telephone: {self.telephone}, nit: {self.nit}, created_at: {self.created_at}"
+  
+  @classmethod
+  @transaction.atomic
+  def create_office(cls, name, address, telephone, nit):
+    office = cls(name=name, address=address, telephone=telephone, nit=nit)
+    office.save()
+    return office
+  
+class CompanyPosition(models.Model):
+  name = models.CharField(max_length=60)
+  created_at = models.DateTimeField(auto_now_add=True)
 
+
+  def __str__(self):
+    return f"id: {self.id}, name: {self.name}, created_at: {self.created_at}"
+  
+  @classmethod
+  @transaction.atomic
+  def create_company_position(cls, name):
+    company_position = cls(name=name)
+    company_position.save()
+    return company_position
