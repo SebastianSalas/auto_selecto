13fe006d8310cc17d6a4a9289e8da3f9c941a963
CarlosHernandez93 <64928869+CarlosHernandez93@users.noreply.github.com>
2023-06-30 12:12:48 -0500

    Creacion de la pagina para mostrar los carros

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,6 +8,7 @@ import SignupPage from "./pages/SignupPage";
 import AuthContext from "./context/AuthContext";
 import StaffMembersIndexPage from "./pages/StaffMembersIndexPage";
 import { useContext } from "react";
+import ShowCarsPage from "./pages/ShowCarsPage";
 
 export default function App() {
   let { user } = useContext(AuthContext);
@@ -20,6 +21,7 @@ export default function App() {
       <Route path="/login" element={!user ? <LoginPage /> : <Navigate to="/" />} />
       <Route path="/sign_up" element={!user ? <SignupPage /> : <Navigate to="/" />} />
       <Route path="/staff_members" element={<StaffMembersIndexPage/>}/>
+      <Route path="/show_cars" element={<ShowCarsPage/>}/>
       <Route path="*" element={<NotFoundPage />} />
     </Routes>
   );
