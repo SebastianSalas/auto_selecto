301e70e1e14a8fa304e062c8cfbc5b2c23bd46ab
Juan Camilo Ortiz <juancortizgonz@gmail.com>
2023-10-22 11:56:12 -0500

    Update CI file and add new test

--- auto_selecto/users/tests.py
+++ auto_selecto/users/tests.py
@@ -31,3 +31,36 @@ class UserTestCase(TestCase):
         )
 
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+
+    def test_get_user_not_exists(self):
+        client = APIClient()
+        response = client.get(
+            '/api/client/1/' # The user doesn't exist
+        )
+
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+
+    # def test_get_user(self):
+    #     client = APIClient()
+    #     create_client = client.post(
+    #         '/api/client/create/', {
+    #             'email': 'test_create_user@test.com',
+    #             'name': 'Test Name',
+    #             'last_name': 'Test LastName',
+    #             'cedula': '000000',
+    #             'password': 'password',
+    #             'telephone': '333222111'
+    #         },
+    #         format='json'
+    #     )
+
+    #     if (create_client.status_code == status.HTTP_201_CREATED):
+    #         response = client.get(
+    #             '/api/client/1/' # The user doesn't exist
+    #         ) 
+    #     else:
+    #         response = status.HTTP_404_NOT_FOUND
+
+    #     self.assertEqual(response.status_code, status.HTTP_200_OK)
