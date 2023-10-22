4ebf572b9a48098d717c4aa759481b1dfa07799c
Juan Camilo Ortiz <juancortizgonz@gmail.com>
2023-10-22 10:57:43 -0500

    CI initial config

--- auto_selecto/users/tests.py
+++ auto_selecto/users/tests.py
@@ -1,3 +1,33 @@
+# Django
 from django.test import TestCase
 
-# Create your tests here.
+# Models
+from .models import User, Client, StaffMember
+
+# Django Rest Framework
+from rest_framework.test import APIClient
+from rest_framework import status
+
+class UserTestCase(TestCase):
+    def setUp(self):
+        user = User(
+            name='Test Name',
+            last_name='Test LastName',
+            cedula='000000'
+        )
+    
+    def test_create_user(self):
+        client = APIClient()
+        response = client.post(
+            '/api/client/create/', {
+                'email': 'test_create_user@test.com',
+                'name': 'Test Name',
+                'last_name': 'Test LastName',
+                'cedula': '000000',
+                'password': 'password',
+                'telephone': '333222111'
+            },
+            format='json'
+        )
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
