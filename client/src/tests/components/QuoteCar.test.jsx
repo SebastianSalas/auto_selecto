120ba88d88dae0ca8bd52f963b866c25333be46b
Velyxius <dadawdasd@hotmail.com>
2023-12-16 18:26:16 -0500

    Added some client unit tests

--- client/src/tests/components/QuoteCar.test.jsx
+++ client/src/tests/components/QuoteCar.test.jsx
@@ -4,10 +4,16 @@ import { render, screen } from "@testing-library/react";
 
 describe("QuoteCar", () => {
   beforeEach(() => {
-    render(<QuoteCar />);
+    render(
+      <QuoteCar brand="Chevrolet" name="Spark" year="2023" value="1000" />
+    );
   });
 
-  test("Should render QuoteCar", () => {
+  test("Should render QuoteCar with props correctly", () => {
     expect(screen.getByText(/Vehiculo a cotizar/i)).toBeDefined();
+    expect(screen.getByText(/Chevrolet/i)).toBeDefined();
+    expect(screen.getByText(/Spark/i)).toBeDefined();
+    expect(screen.getByText(/2023/i)).toBeDefined();
+    expect(screen.getByText(/1,000/i)).toBeDefined();
   });
 });
