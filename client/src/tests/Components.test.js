e84dcd79b53d5679a3eed3c9c9b3ddb579ae0e7c
Juan Camilo Ortiz <juancortizgonz@gmail.com>
2023-12-16 11:01:02 -0500

    Update render deployment

---
+++ client/src/tests/Components.test.js
@@ -0,0 +1,10 @@
+/* eslint-disable no-unused-vars */
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import '@testing-library/jest-dom';
+
+import App from '../App';
+
+// test('Component Navbar rendered!', () => {
+//     render(<App />);
+// })
